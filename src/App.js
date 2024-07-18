17f970ffce1c2b7fdfc75e635f7e34bd6234db3a
Pijus Bilinskas <pijus.bilins@gmail.com>
2024-07-18 14:35:45 +0300

    added assests from figma and made the navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 
+import { Footer, Blog, Possibility, Features, WhatGPT, Header } from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import "./App.css"
+
 const App = () => {
     return (
-        <div>
-            <h1>FEP-Pijus</h1>
+        <div className="App">
+            <div className="gradient__bg">
+            <Navbar />
+            <Header />
+            </div>
+            <Brand />
+            <WhatGPT />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
